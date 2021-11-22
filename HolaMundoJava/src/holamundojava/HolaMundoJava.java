da8cba65976a7ccd0ea13fb4ecf043a0b1d97d8f
carlos.sancho.sanfelix@gmail.com <>
2021-11-22 19:25:06 +0100

    Esto es una prueba para probar como funciona

---
+++ HolaMundoJava/src/holamundojava/HolaMundoJava.java
@@ -0,0 +1,24 @@
+/*
+ * Proyecto HolaMundoJava - Archivo HolaMundoJava.java - Compañía DAW
+ * Licencia Creative Commons BY-NC-SA 4.0
+ * https://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
+
+package holamundojava;
+
+/**
+ *
+ * @author Carlos Sancho Sanfélix <carlos.sancho.sanfelix@gmail.com>
+ * @version 1.0
+ * @date 22 nov. 2021 19:23:34
+ */
+public class HolaMundoJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
