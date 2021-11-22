5d810f9ae0b9bd33eef46dd21992ce3b053b8df2
carlos.sancho.sanfelix@gmail.com <a033564158m@S09-PC07>
2021-11-22 19:27:27 +0100

    Esto es una prueba para probar como funciona, he agregado Hola Mundo.

--- HolaMundoJava/src/holamundojava/HolaMundoJava.java
+++ HolaMundoJava/src/holamundojava/HolaMundoJava.java
@@ -18,7 +18,7 @@ public class HolaMundoJava {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola Mundo.");
     }
 
 }
